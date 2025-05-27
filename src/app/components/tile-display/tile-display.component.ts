528ec48d88459bc93fc64dc6259e530bdfb68a2e
Falko Langmack <falko.langmack@landdata.de>
2025-05-27 15:27:41 +0200

    Tile Data Displayed

--- src/app/components/tile-display/tile-display.component.ts
+++ src/app/components/tile-display/tile-display.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Tile} from '../../domain/model/type.type';
+import {PanelComponent} from '../panel/panel.component';
 
 @Component({
   standalone: true,
   selector: 'app-tile-display',
-  imports: [],
+  imports: [
+    PanelComponent
+  ],
   templateUrl: './tile-display.component.html',
   styleUrl: './tile-display.component.scss'
 })

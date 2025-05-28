f69b50c1f99f8d5bafb2b07c26ba027c631af5de
HC307 <langmackf@gmail.com>
2025-05-29 01:36:55 +0200

    Refactoring

--- src/app/features/compendium/compendium.component.ts
+++ src/app/features/compendium/compendium.component.ts
@@ -1,29 +1,34 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Route, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  Route,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { PanelComponent } from '../../components/panel/panel.component';
 
 @Component({
   selector: 'app-compendium',
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive
-  ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './compendium.component.html',
-  styleUrl: './compendium.component.scss'
+  styleUrl: './compendium.component.scss',
 })
 export class CompendiumComponent implements OnInit {
   childRoutes: { path: string; title?: string }[] = [];
 
-  constructor(private route: ActivatedRoute, private router: Router) {
-  }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    const currentRoute = this.router.config.find(r => r.path === 'compendium');
+    const currentRoute = this.router.config.find(
+      (r) => r.path === 'compendium'
+    );
 
     if (currentRoute && currentRoute.children) {
       this.childRoutes = currentRoute.children.map((child: Route) => ({
         path: child.path!,
-        title: child.title ? child.path : 'missing route title' // fallback to path if title missing
+        title: child.title ? child.path : 'missing route title', // fallback to path if title missing
       }));
     }
   }
